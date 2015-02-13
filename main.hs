cc332fbcca19aa20906d692cd20f52a84fa1be19
hengchu <hengchu.zhang@gmail.com>
2015-02-13 17:34:51 -0500

    made main a bit more robust

--- main.hs
+++ main.hs
@@ -2,19 +2,28 @@ import Parser
 import Trans
 import Eval
 import Pretty
+import Control.Exception
 import GHC.IO.Handle
 import GHC.IO.Handle.FD
 
 loop :: IO ()
 loop = do putStr "> "
           hFlush stdout
-          input <- getLine
-          case parseInput "<stdin>" input of
-            Left  _  -> return ()
-            Right toks -> prettyprint $ eval $ fst $ trans toks
-          putStr "\n"
-          hFlush stdout
-          loop
+          ineof <- isEOF
+          if not ineof then
+            do input <- getLine
+               case parseInput "<stdin>" input of
+                 Left  err  -> error $ show err
+                 Right toks -> prettyprint $ eval $ fst $ trans toks
+               putStr "\n"
+               hFlush stdout
+               loop
+          else
+            do putStrLn ""
+               return ()
+
+loop' :: IO ()
+loop' = loop `catch` (\e -> do {print (e::Control.Exception.SomeException); loop'})
 
 main :: IO ()
-main = loop
+main = loop' 
